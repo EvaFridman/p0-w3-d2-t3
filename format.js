70507348266caca96098163c2da0809a3014e9c1
Eva Fridman <fridman.1999@mail.ru>
2026-06-30 10:55:52 +0300

    release

---
+++ format.js
@@ -0,0 +1,37 @@
+// Строка с разделением разрядов и знаком валюты
+function formatPrice(number) {
+    if (typeof number !== "number" || isNaN(number)) {
+        return "Error! Price has to be a number";
+    };
+    const formatter = new Intl.NumberFormat("ru-RU", {
+        style: "currency",
+        currency: "RUB",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2
+    });
+    return formatter.format(number);
+}
+console.log(formatPrice(1000));
+console.log(formatPrice(500.5));
+console.log(formatPrice(200000.6785));
+
+// Имя с заглавной первой буквы, остальные строчные
+function formatName(string) {
+    if (typeof string !== 'string' || !string.trim()) return 'Error! Name has to be a string';
+    
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  }
+console.log(formatName("anna"));
+console.log(formatName("jaMEs"));
+console.log(formatName("SaM"));
+
+// Собранное приветствие
+function greet(name, partOfDay) {
+    if (typeof name !== 'string' || !name.trim()) return 'Error! Name has to be a string';
+    if (typeof partOfDay !== 'string' || !partOfDay.trim()) return 'Error! Part of day has to be a string';
+
+    return "Good " + partOfDay + ", " + name + "!";
+}
+console.log(greet("Ana", "morning"));
+console.log(greet("James", "afternoon"));
+console.log(greet("Sam", "evening"));
